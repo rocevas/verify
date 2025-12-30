39d20e70853f4c70b060e7bfd900cb02f63dee78
Sarunas <sarunas@Sarunass-MacBook-Pro-2.local>
2025-12-30 11:36:10 +0200

    laravel jetstream

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,26 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // User::factory(10)->withPersonalTeam()->create();
 
-        User::factory()->create([
-            'name' => 'Sarunas',
-            'email' => 'sarunas@rocevas.lt',
-            'password' => Hash::make('secret'),
-            'email_verified_at' => now(),
+        User::factory()->withPersonalTeam()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
